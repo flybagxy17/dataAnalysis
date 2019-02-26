4cd8f775498f27754ea856b20a9d55ef43a640b6
flybagxy17 <flybagxy185@163.com>
2019-02-26 10:13:49 +0800

    new

---
+++ src/main/scala/HdfsTest.scala
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+import org.apache.spark.sql.SparkSession
+
+
+object HdfsTest {
+
+
+  /**
+    * 数据集格式
+    * 217218853,"0","B20181001000003BY1PD",35,"ars699","ars699",62174,-1.20,"2018-10-01 00:00:00","3",1.20,1.20,1,"",0,"","",\N,0.00,"2018-10-01 00:00:03",\N,"PG","57295158-57295158-201-0","",0,0,"美杜莎 2","","",0.00,0,"2018-10-01 00:00:00"
+    */
+
+  /**
+    * 字段说明
+    * 第六个：name '账号', 第八个：netAmount '额度', 第十一个：betAmount '金额', 第十二个：validBetAmount '有效',
+    */
+  /** Usage: HdfsTest [file] */
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder
+      .appName("HdfsTest")
+      .master("local")/*服务器上不用*/
+      .getOrCreate()
+//    val file = spark.read.text(args(0)).rdd
+
+    val data = spark.sparkContext.textFile("file:///E:\\opt\\moudle\\game\\src\\main\\resources\\test.txt")
+    import spark.implicits._
+    val df = data.map(s =>s.split(",")).map(p => Person(
+        p(0), p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8), p(9),
+        p(10), p(11), p(12), p(13), p(14), p(15), p(16), p(17), p(18), p(19),
+        p(20), p(21), p(22),p(23),p(24),p(25),p(26),p(27), p(28),p(29), p(30),
+        p(31))).toDF()
+
+
+
+
+    df.createOrReplaceTempView("person")
+//    val df1 = spark.sql("select * from person ")
+
+//    df1.show()
+
+    /**
+      *
+      *1.  3.每个用户的输赢
+      */
+
+    /**
+      * 1.用Spark计算投注前十名的用户
+      */
+    val df2 = spark.sql("select name,betAmount from person order by betAmount limit 10")
+    df2.show()
+
+
+    /**
+      * 2.每个用户的有效投注
+      */
+
+    val df3 = spark.sql("select name,validBetAmount from person ")
+    df3.show()
+
+    /**
+      * 3.每个用户的输赢
+      */
+    val df4 = spark.sql("select name,netAmount  from person ")
+    df4.show()
+
+
+
+    spark.stop()
+  }
+}
+
---
+++ src/main/scala/Person.scala
@@ -0,0 +1,69 @@
+
+  /**
+  * 数据类型 (需要替换成你的字段)
+  * @param field1
+  * @param field2
+  * @param field3
+  * @param field4
+  * @param field5
+  * @param field6
+  * @param field7
+  * @param field8
+  * @param field9
+  * @param field10
+  * @param field11
+  * @param field12
+  * @param field13
+  * @param field14
+  * @param field15
+  * @param field16
+  * @param field17
+  * @param field18
+  * @param field19
+  * @param field20
+  * @param field21
+  * @param field22
+  * @param field23
+  * @param field24
+  * @param field25
+  * @param field26
+  * @param field27
+  * @param field28
+  * @param field29
+  * @param field30
+  * @param field31
+  */
+case class Person(
+                 field1:String,
+                 field2:String,
+                 field3:String,
+                 field4:String,
+                 field5:String,
+                 name:String,
+                 field7:String,
+                 netAmount:String,
+                 field9:String,
+                 field10:String,
+                 betAmount:String,
+                 validBetAmount:String,
+                 field13:String,
+                 field14:String,
+                 field15:String,
+                 field16:String,
+                 field17:String,
+                 field18:String,
+                 field19:String,
+                 field20:String,
+                 field21:String,
+                 field22:String,
+                 field23:String,
+                 field24:String,
+                 field25:String,
+                 field26:String,
+                 field27:String,
+                 field28:String,
+                 field29:String,
+                 field30:String,
+                 field31:String,
+                 field32:String
+                 )
